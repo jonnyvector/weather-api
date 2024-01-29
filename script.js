a6c26d589765429a238ab8ed4e88ebb838072f96
JONATHAN HICKS <jonny@teachon.org>
2024-01-29 16:55:43 +0700

    day function and populate forecast day function

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ const cityZip = document.getElementById("city-zip");
 const submitButton = document.querySelector("button");
 const cityName = document.getElementsByClassName("city-name")[0];
 const temperature = document.getElementsByClassName("temperature")[0];
-const apiKey = "87f395a78bc64e53ab860605242601";
+const forecastWeatherContainer = document.getElementsByClassName(
+  "forecast-weather-container"
+);
+const forecastBlockOne = document.getElementById("forecast-block-1");
+const forecastBlockTwo = document.getElementById("forecast-block-2");
+const forecastBlockThree = document.getElementById("forecast-block-3");
+const forecastBlockFour = document.getElementById("forecast-block-4");
 
-console.log(submitButton);
+const apiKey = "87f395a78bc64e53ab860605242601";
 
 async function getWeather() {
   try {
@@ -13,6 +19,8 @@ async function getWeather() {
     );
     const data = await response.json();
     console.log(data);
+    populateWeather(data);
+    getDayLoop(data.forecast.forecastday);
   } catch (error) {
     alert(
       `We couldn't find that city or zip code. Please enter a valid city name, US zip code, or UK zip code.`
@@ -23,9 +31,52 @@ async function getWeather() {
 submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   getWeather();
+  populateForecastDays();
 });
 
 function populateWeather(data) {
   cityName.innerHTML = data.location.name;
   temperature.innerHTML = data.current.temp_f;
+}
+
+function getDay(date) {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const d = new Date(date);
+  let day = weekday[d.getDay()];
+  return day;
+}
+
+const dayArray = [];
+
+function getDayLoop(forecastArray) {
+  forecastArray.forEach((day) => {
+    dayArray.push(getDay(day.date));
+  });
+}
+
+const forecastBlockArray = [
+  forecastBlockOne,
+  forecastBlockTwo,
+  forecastBlockThree,
+  forecastBlockFour,
+];
+
+function populateForecastDays() {
+  console.log(forecastWeatherContainer[0]);
+  dayArray.forEach((day) => {
+    const dayName = document.createElement("p");
+    console.log(day);
+    dayName.textContent = day;
+    console.log(dayName);
+    console.log(forecastWeatherContainer[0]);
+    forecastWeatherContainer[0].appendChild(dayName);
+  });
 }
